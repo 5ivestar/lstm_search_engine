5857170d58ea15f05adc3d050a9204e905cfb07d
takashi <ftakashi@jp.ibm.com>
2017-03-12 01:28:03 +0900

    updating main to handle latest model function

--- main.py
+++ main.py
@@ -10,22 +10,25 @@ import data_handler as dh
 logging.basicConfig(level=logging.INFO,format="%(asctime)s %(levelname)-7s %(funcName)s %(lineno)d %(message)s")
 
 w2v_model_file_name="model/w2v_model/w2v_wiki.mod"
-#kc_data_file_name="data/kc_data_small.csv"
-kc_data_file_name="data/kc_data.csv"
+kc_data_file_name="data/kc_data_small.csv"
+#kc_data_file_name="data/kc_data.csv"
 
 w2v_model=w2v_handler.load_word2vec_model(w2v_model_file_name,"","")
 data_handler=dh.KcDataHandler(kc_data_file_name,stm.SimpleStemmer(),w2v_model)
 querys,documents,vector_cache=data_handler.make_training_data()
 print(len(querys),len(documents),list(vector_cache.keys())[:10])
 
-training_model=lstm_model.LstmSearchModel(mode="training")
+doc_size=len(documents)
+training_model=lstm_model.LstmSearchModel(doc_size,mode="training")
 training_model.set_training_data(querys,documents)
-training_model.train(1000)
-print(training_model.get_doc_vectors())
+training_model.train(200)
+doc_vecs=training_model.get_doc_vectors()
 training_model.close_session()
 
-#restored=lstm_model.LstmSearchModel(mode="query_processing")
-#print(restored.get_query_vector(querys[0]))
+qp_model=lstm_model.LstmSearchModel(doc_size,mode="query_processing",doc_vecs=doc_vecs)
+print(qp_model.get_matching_vector(querys[0]))
+print(qp_model.get_matching_vector(querys[1]))
+print(qp_model.get_matching_vector(querys[3]))
 
 
 
