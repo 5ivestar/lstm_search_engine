8c71314cc1ec2de7751fda63736f5aaa3c9bdaa6
takashi <ftakashi@jp.ibm.com>
2017-03-12 10:07:43 +0900

    modify word2vec loader to rasse exception when invalid arguments

--- w2v_handler.py
+++ w2v_handler.py
@@ -4,9 +4,17 @@ import os
 def load_word2vec_model(model_file,corpus_file,vector_size=200):
     if os.path.exists(model_file):
         model= word2vec.Word2Vec.load(model_file)
-    else:
+    elif os.path.exists(corpus_file):
         data =word2vec.Text8Corpus(corpus_file)
         model=word2vec.Word2Vec(data,size=vector_size)
         model.save(model_file)
     
+    else:
+        raise Exception("neither model_file nor corpus file doesn't exists")
+    
     return model
+
+if __name__=="__main__":
+    corpus_file="data/wiki_2015_knowledgecenter.txt"
+    model_file="wiki_kc_mix"
+    load_word2vec_model(model_file,corpus_file)
